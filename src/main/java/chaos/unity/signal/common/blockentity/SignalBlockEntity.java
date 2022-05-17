641aae773d760be654159841e0daa4708376c73e
Kyle Lin <minecraft.kyle.train@gmail.com>
2022-05-17 10:45:39 +0800

    SignalBlockEntity: Tweak block pos de/serialization

--- src/main/java/chaos/unity/signal/common/blockentity/SignalBlockEntity.java
+++ src/main/java/chaos/unity/signal/common/blockentity/SignalBlockEntity.java
@@ -4,6 +4,7 @@ import chaos.unity.signal.common.util.Utils;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtHelper;
 import net.minecraft.util.math.BlockPos;
 
 public class SignalBlockEntity extends BlockEntity {
@@ -17,18 +18,18 @@ public class SignalBlockEntity extends BlockEntity {
     @Override
     public void readNbt(NbtCompound nbt) {
         if (nbt.contains("rail_bound_pos"))
-            railBindPos = Utils.fromIntArray(nbt.getIntArray("rail_bound_pos"));
+            railBindPos = NbtHelper.toBlockPos(nbt.getCompound("rail_bound_pos"));
         if (nbt.contains("paired_signal_pos"))
-            pairedSignalPos = Utils.fromIntArray(nbt.getIntArray("paired_signal_pos"));
+            pairedSignalPos = NbtHelper.toBlockPos(nbt.getCompound("paired_signal_pos"));
         super.readNbt(nbt);
     }
 
     @Override
     protected void writeNbt(NbtCompound nbt) {
         if (railBindPos != null)
-            nbt.putIntArray("rail_bound_pos", Utils.asIntArray(railBindPos));
+            nbt.put("rail_bound_pos", NbtHelper.fromBlockPos(railBindPos));
         if (pairedSignalPos != null)
-            nbt.putIntArray("paired_signal_pos", Utils.asIntArray(pairedSignalPos));
+            nbt.put("paired_signal_pos", NbtHelper.fromBlockPos(pairedSignalPos));
         super.writeNbt(nbt);
     }
 }
