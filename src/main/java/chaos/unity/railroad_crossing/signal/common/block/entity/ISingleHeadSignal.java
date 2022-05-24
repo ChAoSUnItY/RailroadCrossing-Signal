878b766287608039d11c167e26cdde99fe29af35
Kyle Lin <minecraft.kyle.train@gmail.com>
2022-05-24 15:54:12 +0800

    Make ISingleHeadSignal not sealed

--- src/main/java/chaos/unity/railroad_crossing/signal/common/block/entity/ISingleHeadSignal.java
+++ src/main/java/chaos/unity/railroad_crossing/signal/common/block/entity/ISingleHeadSignal.java
@@ -3,6 +3,6 @@ package chaos.unity.railroad_crossing.signal.common.block.entity;
 import chaos.unity.railroad_crossing.signal.common.data.SignalMode;
 import org.jetbrains.annotations.NotNull;
 
-public sealed interface ISingleHeadSignal permits DistantSingleHeadSignalBlockEntity, SingleHeadSignalBlockEntity {
+public interface ISingleHeadSignal {
     @NotNull SignalMode getSignal();
 }
